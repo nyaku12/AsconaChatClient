60ae4e4e34bfbaab22ead9e8e4f3e872ea39889c
nyaku12 <sukablyatmp4@gmail.com>
2025-09-11 22:40:11 +0500

    a

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,6 +37,7 @@ dependencies {
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
+    implementation("com.google.android.exoplayer:extension-okhttp:2.19.1")
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
---
+++ app/src/main/java/com/example/asconachat/WebSocket/WebSocketClient.java
@@ -0,0 +1,7 @@
+package com.example.asconachat.WebSocket;
+
+import okhttp3.WebSocketListener;
+
+public class WebSocketClient extends WebSocketListener {
+    
+}
